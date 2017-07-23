2672ab7b8ae6438456461d5f00f23ae8acb5a256
arvidn <arvid@cs.umu.se>
2017-07-22 22:01:14 -0700

    fix

--- bindings/python/src/converters.cpp
+++ bindings/python/src/converters.cpp
@@ -294,7 +294,7 @@ void bind_converters()
     to_python_converter<lt::peer_flags_t, from_bitfield_flag<lt::peer_flags_t>>();
     to_python_converter<lt::peer_source_flags_t, from_bitfield_flag<lt::peer_source_flags_t>>();
     to_python_converter<lt::bandwidth_state_flags_t, from_bitfield_flag<lt::bandwidth_state_flags_t>>();
-    to_python_converter<lt::file_open_mode, from_bitfield_flag<lt::file_open_mode>>();
+    to_python_converter<lt::file_open_mode_t, from_bitfield_flag<lt::file_open_mode_t>>();
 
     // work-around types
     to_python_converter<lt::aux::noexcept_movable<lt::address>, address_to_tuple<
@@ -341,5 +341,5 @@ void bind_converters()
     to_bitfield_flag<lt::peer_flags_t>();
     to_bitfield_flag<lt::peer_source_flags_t>();
     to_bitfield_flag<lt::bandwidth_state_flags_t>();
-    to_bitfield_flag<lt::file_open_mode>();
+    to_bitfield_flag<lt::file_open_mode_t>();
 }
