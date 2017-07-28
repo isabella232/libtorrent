cb6e08a979f6f39e72a7553382f15abf3d7918c1
Joseph Petroske <josephp@josephpMBP13.local>
2017-07-28 15:54:09 -0700

    changed build script

--- mk.sh
+++ mk.sh
@@ -62,58 +62,24 @@ mkdir -p builds
 
 if [[ $LINUX = true ]]
 then
-    $BOOST_ROOT/b2 -sBOOST_ROOT="$BOOST_ROOT" -q $BUILD_VARIANT target-os=linux --address-model=64 --build-dir=builds/linux64
-    $BOOST_ROOT/b2 -sBOOST_ROOT="$BOOST_ROOT" -q $BUILD_VARIANT target-os=linux --address-model=32 --build-dir=builds/linux32
+    $BOOST_ROOT/b2 -sBOOST_ROOT="$BOOST_ROOT" -q $BUILD_VARIANT target-os=linux --address-model=64 --build-dir=$LIBTORRENT_DIR/bin install --libdir=builds/linux64/lib --includedir=builds/include
+    $BOOST_ROOT/b2 -sBOOST_ROOT="$BOOST_ROOT" -q $BUILD_VARIANT target-os=linux --address-model=32 --build-dir=$LIBTORRENT_DIR/bin install --libdir=builds/linux32/lib --includedir=builds/include
 fi
 
 if [[ $OSX = true ]]
 then
-    $BOOST_ROOT/b2 -sBOOST_ROOT="$BOOST_ROOT" -q $BUILD_VARIANT target-os=darwin --address-model=64 --build-dir=builds/osx64
-    $BOOST_ROOT/b2 -sBOOST_ROOT="$BOOST_ROOT" -q $BUILD_VARIANT target-os=darwin --address-model=32 --build-dir=builds/osx32
+    $BOOST_ROOT/b2 -sBOOST_ROOT="$BOOST_ROOT" -q $BUILD_VARIANT target-os=darwin --address-model=64 --build-dir=$LIBTORRENT_DIR/bin install --libdir=builds/osx64/lib --includedir=builds/include
+    $BOOST_ROOT/b2 -sBOOST_ROOT="$BOOST_ROOT" -q $BUILD_VARIANT target-os=darwin --address-model=32 --build-dir=$LIBTORRENT_DIR/bin install --libdir=builds/osx32/lib --includedir=builds/include
 fi
 
 if [[ $WINDOWS = true ]]
 then
-    $BOOST_ROOT/b2 -sBOOST_ROOT="$BOOST_ROOT" -q $BUILD_VARIANT target-os=windows --address-model=64 --build-dir=builds/win64
-    $BOOST_ROOT/b2 -sBOOST_ROOT="$BOOST_ROOT" -q $BUILD_VARIANT target-os=windows --address-model=32 --build-dir=builds/win32
+    $BOOST_ROOT/b2 -sBOOST_ROOT="$BOOST_ROOT" -q $BUILD_VARIANT target-os=windows --address-model=64 --build-dir=$LIBTORRENT_DIR/bin install --libdir=builds/windows64/lib --includedir=builds/include
+    $BOOST_ROOT/b2 -sBOOST_ROOT="$BOOST_ROOT" -q $BUILD_VARIANT target-os=windows --address-model=32 --build-dir=$LIBTORRENT_DIR/bin install --libdir=builds/windows32/lib --includedir=builds/include
 fi
 
 if [[ $ZIP = true ]]
 then
-    # forces us to update everything, and remove things that no longer exist
-    rm -rf builds/lib/linux64
-    rm -rf builds/lib/linux32
-    rm -rf builds/lib/osx64
-    rm -rf builds/lib/osx32
-    rm -rf builds/lib/windows64
-    rm -rf builds/lib/windows32
-    rm -rf builds/include
-
-    mkdir -p builds/include
-    mkdir -p builds/lib
-    
-    mkdir -p builds/lib/linux64
-    mkdir -p builds/lib/linux32
-    mkdir -p builds/lib/osx64
-    mkdir -p builds/lib/osx32
-    mkdir -p builds/lib/win64
-    mkdir -p builds/lib/win32
-    
-    cp `find builds/linux64 -iname "*.a"` builds/lib/linux64
-    cp `find builds/linux64 -iname "libtorrent.so*"` builds/lib/osx64
-    cp `find builds/linux32 -iname "*.a"` builds/lib/linux32
-    cp `find builds/linux32 -iname "libtorrent.so*"` builds/lib/osx64
-
-    cp `find builds/osx64 -iname "*.a"` builds/lib/osx64
-    cp `find builds/osx64 -iname "libtorrent.dylib*"` builds/lib/osx64
-    cp `find builds/osx32 -iname "*.a"` builds/lib/osx32
-    cp `find builds/osx32 -iname "libtorrent.dylib*"` builds/lib/osx64
-
-    cp `find builds/win64 -iname "*.a"` builds/lib/win64
-    cp `find builds/win64 -iname "*.dll*"` builds/lib/win64
-    cp `find builds/win32 -iname "*.a"` builds/lib/win32
-    cp `find builds/win32 -iname "*.dll*"` builds/lib/win32    
-    
-    cp -R $LIBTORRENT_DIR/include/libtorrent/* builds/include
+     cp -R $LIBTORRENT_DIR/include/libtorrent/* builds/include
     ( cd builds ; zip builds.zip include lib )
 fi
